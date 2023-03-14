e1d4ee70f84ad0297ad5faa1ca60d0e16a0ae018
Heluhan <m17501641586@gmail.com>
2023-03-14 03:02:36 -0400

    Add error handling and mongoose model

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,29 @@
 import "dotenv/config.js"
-import express from "express";
+import express, { NextFunction , Request, Response} from "express";
+import NoteModel from "./models/note"
 
 
 const app = express();
-app.get("/", (req, res) => {
-    res.send("Hello, World!")
+app.get("/", async (req, res, next) => {
+    try {
+        const notes = await NoteModel.find().exec()
+        res.status(200).json(notes)
+    } catch (error) {
+        next(error);
+    }
+
+})
+
+app.use((req, res, next) => {
+    next(Error("Endpoint not found"))
+})
+
+
+app.use((error: unknown, req:Request, res:Response, next:NextFunction) => {
+    console.error(error)
+    let errMessage = "An unknown error occurred"
+    if (error instanceof Error) errMessage = error.message;
+    res.status(500).json({error: errMessage})
 })
 
 
